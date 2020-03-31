1fb091829b1ceb62cdd16310b90ff32b962c9dfc
Apple <apple@Apples-MacBook-Pro-2.local>
2020-03-31 12:20:05 +0700

    add test api line

---
+++ dags/config/news_config.py
@@ -0,0 +1,10 @@
+news_config = {
+   "news_schedule_interval":"@daily",
+   "news_base_url":"https://newsapi.org/v2/top-headlines",
+   "news_api_key":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+   "news_countries":[
+      "th",
+      "us"
+   ],
+   "line_notify":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
